cde8b2cd56d28ad1c18b08608bb2ec8d83621209
Vadim Constantin <vad.constantin@gmail.com>
2022-04-14 01:10:08 +0200

    created model transaction and seed is ready to parse csv file

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_04_13_230540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "transactions", force: :cascade do |t|
+    t.date "date"
+    t.integer "order_id"
+    t.integer "customer_id"
+    t.string "country"
+    t.string "product_code"
+    t.string "product_description"
+    t.integer "quantity"
+    t.float "unit_price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'csv'
+
+CSV.foreach('lib/memory-tech-challenge-data.csv', headers: true) do |row|
+
+  Transaction.create({
+    date: row[0],
+    order_id: row[1],
+    customer_id: row[2],
+    country: row[3],
+    product_code: row[4],
+    product_description: row[5],
+    quantity: row[6],
+    unit_price: row[7]
+  })
+end
+
+puts "seed is completed ! :) "
