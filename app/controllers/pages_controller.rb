e545af8e2b64d92147971b0fbc827fa4786b74a8
Vadim Constantin <vad.constantin@gmail.com>
2022-04-14 01:26:44 +0200

    total revenue for card 1 done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,14 @@
 class PagesController < ApplicationController
   def home
+
+    #initialisation de toutes les transactions
+    @transactions = Transaction.all
+
+    #calcul du montant total
+    @total = 0
+    @transactions.each do |trans|
+      @total += (trans.unit_price * trans.quantity)
+    end
+
   end
 end
