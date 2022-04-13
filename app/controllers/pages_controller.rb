cba41ab242a89549cd7e4911775296df21c78595
Vadim Constantin <vad.constantin@gmail.com>
2022-04-14 01:38:13 +0200

    number of unique customers for card 3 is done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,9 +13,15 @@ class PagesController < ApplicationController
     #calcul de la moyenne par order
     unique_orders = count_unique_orders(@transactions)
     @average_revenue_per_order = @total.fdiv(unique_orders)
+
+    #calcul du nombre de clients uniques
+    @unique_customers = count_unique_customers(@transactions)
+
   end
 
+
   private
+
 
   def count_unique_orders(transactions)
     array_of_orders = []
@@ -25,6 +31,14 @@ class PagesController < ApplicationController
 
     # methode pour compter le nombre d'élements uniques dans un array. On regroupe, on map, on convertit en hash avec clé: element, valeur : récurrence, puis compter le nombre de clés avec .length
     return array_of_orders.group_by{|e| e}.map{|k, v| [k, v.length]}.to_h.length
+  end
+
+  def count_unique_customers(transactions)
+    array_of_customers = []
+    transactions.each do |t|
+      array_of_customers << t.customer_id
+    end
+    return array_of_customers.group_by{|e| e}.map{|k, v| [k, v.length]}.to_h.length
   end
 
 
