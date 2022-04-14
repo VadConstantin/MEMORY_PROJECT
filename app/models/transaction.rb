4b74ae81908cf0b25c773affca6100ec886b7aa0
Vadim Constantin <vad.constantin@gmail.com>
2022-04-14 02:38:29 +0200

    filter working on cards

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,8 @@
 class Transaction < ApplicationRecord
+  include PgSearch::Model
+  pg_search_scope :search_by_country,
+    against: [ :country],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
