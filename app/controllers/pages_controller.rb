a9598459e94984140cf89722b9667f5c398c8c33
Vadim Constantin <vad.constantin@gmail.com>
2022-07-01 23:22:43 +0200

    SQL requests really optimized

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,47 +1,26 @@
 # rubocop:disable Metrics/MethodLength
+# rubocop:disable Style/MultilineTernaryOperator
 class PagesController < ApplicationController
 
   def home
 
-    # filtres avec PG search
-    params[:country].present? ? transactions = Transaction.search_by_country(params[:country]).reorder('') : transactions = Transaction.all
-    transactions = Transaction.all if params[:country] == "All"
+    params[:country].present? ?
+      transactions = Transaction.filtered(params[:country])
+    : transactions = Transaction.all
 
-    # trouver tous les pays
-    all_transactions = Transaction.all
-    @countries = count_unique_countries(all_transactions)
+    @countries = Transaction.countries.map do |country|
+      country.country
+    end
 
-    # calcul du montant total
-    @total = Transaction.total
-
-    # calcul de la moyenne par order
-    @unique_orders = Transaction.unique_orders
-    # unique_orders = count_unique_orders(transactions)
-    # @average_revenue_per_order = @total.fdiv(unique_orders) if unique_orders != 0
-    @average_revenue_per_order = Transaction.average_revenue_per_order
-
-    # calcul du nombre de clients uniques
-    @unique_customers = count_unique_customers(transactions)
+    @total = transactions.total
+    @unique_orders = transactions.unique_orders
+    @average_revenue_per_order = transactions.average_revenue_per_order
+    @unique_customers = transactions.unique_customers
 
     # graph
     @graph_transactions = transactions.group_by_month(:date, format: "%B %Y").sum('quantity * unit_price')
   end
 
-  private
-
-  def count_unique_orders(transactions)
-    array_of_orders = transactions.map {|transaction| transaction.order_id}
-    return array_of_orders.uniq.length
-  end
-
-  def count_unique_customers(transactions)
-    array_of_customers = transactions.map {|transaction| transaction.customer_id}
-    return array_of_customers.uniq.length
-  end
-
-  def count_unique_countries(transactions)
-    array_of_countries = transactions.map {|transaction| transaction.country}
-    return array_of_countries.group_by {|country| country}.map {|k, v| [k]}.flatten
-  end
 end
 # rubocop:enable Metrics/MethodLength
+# rubocop:enable Style/MultilineTernaryOperator
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,12 +1,9 @@
 class Transaction < ApplicationRecord
   include PgSearch::Model
   pg_search_scope :search_by_country,
-    against: [:country],
-    using: {
-      tsearch: { prefix: true }
-    }
-
-  # scope :orders, -> {where()}
+                  against: [:country],
+                  using: { tsearch: { prefix: true } }
+  scope :filtered, ->(filter) { where(country: filter) }
 
   class << self
     def total
@@ -18,10 +15,15 @@ class Transaction < ApplicationRecord
     end
 
     def average_revenue_per_order
-      self.sum("transactions.quantity * transactions.unit_price").fdiv(self.select(:order_id).distinct.count)
+      self.sum("(transactions.quantity * transactions.unit_price)").fdiv(self.select(:order_id).distinct.count)
     end
 
+    def unique_customers
+      self.select(:customer_id).distinct.count
+    end
 
+    def countries
+      self.select(:country).distinct
+    end
   end
-
 end
