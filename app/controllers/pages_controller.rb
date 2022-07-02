9c1c600a02191f114588cf825a5e6c152e3c12ef
Vadim Constantin <vad.constantin@gmail.com>
2022-07-02 16:28:48 +0200

    cleaned the code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,26 +1,22 @@
 # rubocop:disable Metrics/MethodLength
-# rubocop:disable Style/MultilineTernaryOperator
+
 class PagesController < ApplicationController
 
   def home
-
-    params[:country].present? ?
+    if params[:country].present?
       transactions = Transaction.filtered(params[:country])
-    : transactions = Transaction.all
-
-    @countries = Transaction.countries.map do |country|
-      country.country
+    else
+      transactions = Transaction.all
     end
 
-    @total = transactions.total
-    @unique_orders = transactions.unique_orders
-    @average_revenue_per_order = transactions.average_revenue_per_order
-    @unique_customers = transactions.unique_customers
+    @countries = Transaction.countries.map(&:country)
 
-    # graph
-    @graph_transactions = transactions.group_by_month(:date, format: "%B %Y").sum('quantity * unit_price')
+    @total                     = transactions.total
+    @unique_orders             = transactions.unique_orders
+    @average_revenue_per_order = transactions.average_revenue_per_order
+    @unique_customers          = transactions.unique_customers
+    @graph_transactions        = transactions.graph_transactions
   end
 
 end
 # rubocop:enable Metrics/MethodLength
-# rubocop:enable Style/MultilineTernaryOperator
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,27 +3,32 @@ class Transaction < ApplicationRecord
   pg_search_scope :search_by_country,
                   against: [:country],
                   using: { tsearch: { prefix: true } }
+
   scope :filtered, ->(filter) { where(country: filter) }
 
   class << self
     def total
-      self.sum("transactions.quantity * transactions.unit_price")
+      sum("transactions.quantity * transactions.unit_price")
     end
 
     def unique_orders
-      self.select(:order_id).distinct.count
+      select(:order_id).distinct.count
     end
 
     def average_revenue_per_order
-      self.sum("(transactions.quantity * transactions.unit_price)").fdiv(self.select(:order_id).distinct.count)
+      sum("(transactions.quantity * transactions.unit_price)").fdiv(select(:order_id).distinct.count)
     end
 
     def unique_customers
-      self.select(:customer_id).distinct.count
+      select(:customer_id).distinct.count
     end
 
     def countries
-      self.select(:country).distinct
+      select(:country).distinct
+    end
+
+    def graph_transactions
+      group_by_month(:date, format: "%B %Y").sum('quantity * unit_price')
     end
   end
 end
